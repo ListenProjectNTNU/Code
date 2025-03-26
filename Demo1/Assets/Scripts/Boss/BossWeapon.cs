913f269390a02cb2f362ec52bb1ba5ae21dd70e8
jasonchu123 <160808343+jasonchu123@users.noreply.github.com>
2025-03-26 17:12:55 +0800

    fireball

--- Demo1/Assets/Scripts/Boss/BossWeapon.cs
+++ Demo1/Assets/Scripts/Boss/BossWeapon.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossWeapon : MonoBehaviour
 {
-	public int attackDamage = 20;
-	public int enragedAttackDamage = 40;
+		public int attackDamage = 20;
+		public int enragedAttackDamage = 40;
 
-	public Vector3 attackOffset;
-	public float attackRange = 1f;
-	public LayerMask attackMask;
+		public Vector3 attackOffset;
+		public float attackRange = 1f;
+		public LayerMask attackMask;
 
-	public void Attack()
-	{
-		Vector3 pos = transform.position;
-		pos += transform.right * attackOffset.x;
-		pos += transform.up * attackOffset.y;
+		public void Attack()
+		{
+			Vector3 pos = transform.position;
+			pos += transform.right * attackOffset.x;
+			pos += transform.up * attackOffset.y;
 
-		Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+			Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
 		if (colInfo != null)
 		{
 			// 嘗試從 Player 取得 PlayerController
