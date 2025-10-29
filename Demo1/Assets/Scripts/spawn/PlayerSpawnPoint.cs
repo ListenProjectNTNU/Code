8dfdf9bda86e742c13670916ffa51ffaf69e0c59
游建佑 <144083534+yoyoscott87@users.noreply.github.com>
2025-10-29 22:13:04 +0800

    delete trash

--- Demo1/Assets/Scripts/spawn/PlayerSpawnPoint.cs
+++ Demo1/Assets/Scripts/spawn/PlayerSpawnPoint.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerSpawnPoint : MonoBehaviour {
     public string spawnId = "default";
+
     private void OnDrawGizmos() {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, 0.2f);
+
+        #if UNITY_EDITOR
         UnityEditor.Handles.Label(transform.position + Vector3.up * 0.25f, $"Spawn: {spawnId}");
+        #endif
     }
 }
